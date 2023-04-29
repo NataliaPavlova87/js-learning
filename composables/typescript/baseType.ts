251f72e2898051fc7920e7a456cb67469e6eead7
Natalia Pavlova <natalia.pavlova@imedia.ru>
2023-04-29 13:21:32 +0300

    Learn TypeScript

--- composables/typescript/baseType.ts
+++ composables/typescript/baseType.ts
@@ -24,11 +24,34 @@ const s1: symbol = Symbol('hello');
 const b1: bigint = 123n;
 
 // Function
-
 let sumFn: (n1: number, n2: number, callback: (d: any) => void) => number;
 
 sumFn = (a: number, b: number): number => {
     return a + b;
 }
 
+// Union Types Объединенные типы
+const u1: (string | number)[] = [123, 'hello'];
 
+// Literal Types - то, что можно записать в одну строчку
+type Action = 'text' | 'json';
+type person = {age: number, name: string};
+
+// Interfaces
+
+interface Person {
+    age: number
+    name: number
+}
+
+interface Adress {
+    street: string
+}
+
+interface User extends Person, Adress {}
+
+// Type Guard
+
+const isString = (val: string | boolean): val is string => {
+    return typeof val === 'string'
+}
